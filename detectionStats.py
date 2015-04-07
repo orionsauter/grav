5c2b8576a68ffd02939fe356e9e43dc54f56530a
orionsauter <orionsauter@gmail.com>
2015-04-07 15:01:45 -0400

    Percent applies to injections, not search segments
    Still not quite matching plot

--- detectionStats.py
+++ detectionStats.py
@@ -6,7 +6,9 @@ import re
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
+# Read table of injected signals
 def getInjections():
     f = open('injection_list.txt')
     inject = dict()
@@ -21,6 +23,7 @@ def getInjections():
     f.close()
     return inject
 
+# Read DAG file to find frequencies
 def getOutputSegs(dag):
     f = open(dag)
     segments = dict()
@@ -33,6 +36,7 @@ def getOutputSegs(dag):
     f.close()
     return segments
 
+# Traverse output directory
 def getData(direc):
     data = list([])
     patt = re.compile('\d+-\d+\.\d+')
@@ -55,9 +59,13 @@ if __name__ == "__main__":
     segments = getOutputSegs(dag)
     inject = getInjections()
     patt = re.compile('_all')
-    dets = []
-    rats = []
+    dets = np.zeros_like(inject.keys(),dtype=float)
+    rats = np.zeros_like(inject.keys(),dtype=float)
+    i = -1
+    
+    # For each injection, gather output within frequency range
     for injName in inject.keys():
+        i = i + 1
         injInfo = inject[injName]
         freqlo = float(injInfo[19])
         freqhi = float(injInfo[20])
@@ -65,22 +73,34 @@ if __name__ == "__main__":
         for freq in segments.keys():
             if (freq <= freqhi and freq >= freqlo):
                 segList = segList + list([segments[freq]])
+        maxSNR = 0
         for seg in segList:
             data = getData('./output/'+seg)
             for line in data:
+                # Look for UL lines with nonzero ul
                 if (len(line) > 20 and line[1] == 'ul' and float(line[14]) > 0):
-                    rats.append(injInfo[10]/float(line[14]))
+                    ratio = injInfo[10]/float(line[14])
+                    if (float(line[13]) < maxSNR):
+                        continue
+                    rats[i] = ratio
+                    maxSNR = float(line[13])
+                    # Detection if SNR > 7 and freq within 0.002
                     if (patt.search(line[5]) and float(line[13]) > 7 and
                         abs(float(line[7]) - injInfo[6]) < 0.002):
-                        dets.append(1)
-                    else:
-                        dets.append(0)
-    norm, xbins = np.histogram(rats)
+                        dets[i] = 1
+                        
+    # Evenly distribute h0/ul values into bins
+    xbins = stats.mstats.mquantiles(rats,np.arange(11)*0.1)
     nbins = xbins.size - 1
     detmap = np.digitize(rats,xbins)
     dets = np.array(dets)
     percent = np.zeros(nbins)
-    for i in range(nbins): 
-        percent[i] = np.sum(dets[detmap == i])*100.0/norm[i]
+    # Bin detections by h0/ul
+    for i in range(nbins):
+        subset = dets[detmap == i]
+        if (subset.size > 0):
+            percent[i] = np.mean(subset)*100.0
     plt.plot(xbins[0:nbins],percent,'o')
+    plt.xlabel('h0/ul')
+    plt.ylabel('Percent Detected')
     plt.savefig('detection.png',bbox_inches='tight')
